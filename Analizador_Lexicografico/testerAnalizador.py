d0db435929b8be67e97f730f90ad968236f26c52
DanielRS <danielrs9705@gmail.com>
2018-05-13 21:42:52 -0400

    Se agrega la segunda prueba que consiste en captacion de errores. La primera ya paso

--- Analizador_Lexicografico/testerAnalizador.py
+++ Analizador_Lexicografico/testerAnalizador.py
@@ -3,16 +3,27 @@ import unittest
 # Se importa el analizador lexicografico 
 from analizador import Analizador_Lexicografico
 
-analizador_lex = Analizador_Lexicografico()
+
 
 class AnalizadorLexTester(unittest.TestCase):
-    analizador_lex.build()
+   
 
     # Prueba basica. Prueba provista por el enunciado del proyecto 
     def testPrimeraPrueba(self):
+        analizador_lex = Analizador_Lexicografico()
+        analizador_lex.build()
         resultadoEsperado = open("salida1.txt", "r")
         resultadoEsperado = resultadoEsperado.read()
-        self.assertEquals(resultadoEsperado, analizador_lex.test("prueba1.txt"))
+        self.assertEqual(resultadoEsperado, analizador_lex.test("prueba1.txt"))
+
+    # Prueba para ver si es correcto el formato de salida cuando hay errores en el
+    # archivo de entrada.
+    def testCaptandoErrores(self):
+        analizador_lex = Analizador_Lexicografico()
+        analizador_lex.build()
+        resultadoEsperado = open("salida2.txt")
+        resultadoEsperado = resultadoEsperado.read()
+        self.assertEqual(resultadoEsperado, analizador_lex.test("prueba2.txt"))
 
 
 if __name__ == '__main__':
