c96488417242d9c95098b7bec65153e20e211847
DanielRS <danielrs9705@gmail.com>
2018-05-13 20:16:50 -0400

    Se pone el analizador como una clase para poder instanciarlo y usar unittest.

--- Analizador_Lexicografico/analizador.py
+++ Analizador_Lexicografico/analizador.py
@@ -4,194 +4,200 @@ import ply.lex as lex
 
 # Diccionario de palabras reservadas
 
-reserved = {
-	'begin' : 'TkBegin',
-	'if' : 'TkIf',
-	'while' : 'TkWhile',
-	'bool' : 'TkBool',
-	'var' : 'TkVar',
-	'with' : 'TkWith',
-	'int' : 'TkInt',
-	'End' : 'TkEnd',
-	'True': 'TkTrue',
-	'False': 'TkFalse',
-	'not': 'TkNegacion',
-}
+class Analizador_Lexicografico(object):
+	reserved = {
+		'begin' : 'TkBegin',
+		'if' : 'TkIf',
+		'while' : 'TkWhile',
+		'bool' : 'TkBool',
+		'var' : 'TkVar',
+		'with' : 'TkWith',
+		'int' : 'TkInt',
+		'End' : 'TkEnd',
+		'True': 'TkTrue',
+		'False': 'TkFalse',
+		'not': 'TkNegacion',
+	}
 
 
-# Diccionarios de tokens 
+	# Diccionarios de tokens 
 
-tokens = [
-	#--------------------------------------------------------------
-	# Token para variables 
-	'TkId',
+	tokens = [
+		#--------------------------------------------------------------
+		# Token para variables 
+		'TkId',
 
-	# Token para numero
-	'TkNum',
+		# Token para numero
+		'TkNum',
 
-	# Token para caracteres 
-	'TkCaracter',
-	#---------------------------------------------------------------
+		# Token para caracteres 
+		'TkCaracter',
+		#---------------------------------------------------------------
 
-	# Tokens para separadores 
-	'TkComa',               # ","
-	'TkPunto',              # "."
-	'TkDosPuntos',          # ":"
-	'TkParAbre',            # "("
-	'TkParCierra',          # ")"
-	'TkCorcheteAbre',       # "["
-	'TkCorcheteCierra',     # "]"
-	'TkLlaveAbre',          # "{"
-	'TkLlaveCierra',        # "}"
-	'TkHacer',              # "->"
-	'TkAsignacion',         # "<-"
+		# Tokens para separadores 
+		'TkComa',               # ","
+		'TkPunto',              # "."
+		'TkDosPuntos',          # ":"
+		'TkParAbre',            # "("
+		'TkParCierra',          # ")"
+		'TkCorcheteAbre',       # "["
+		'TkCorcheteCierra',     # "]"
+		'TkLlaveAbre',          # "{"
+		'TkLlaveCierra',        # "}"
+		'TkHacer',              # "->"
+		'TkAsignacion',         # "<-"
 
-	# Tokens operadores aritmeticos, booleanos, relacionales, o de otro tipo
-	'TkSuma',               # "+"
-	'TkResta',              # "-"
-	'TkMult',               # "*"
-	'TkDiv',                # "/"
-	'TkMod',                # "%"
-	'TkConjuncion',         # "/\"
-	'TkDisyuncion',         # "\/"
-	'TkMenor',              # "<"
-	'TkMenorIgual',         # "<="
-	'TkMayor',              # ">"
-	'TkMayorIgual',         # ">="
-	'TkIgual',              # "="
-	'TkSiguienteChar',      # "++"
-	'TkAnteriorChar',       # "--"
-	'TkValorAscii',         # "#"
-	'TkConcatenacion',      # "::"
-	'TkShift',              # "$"
-] + list(reserved.values())
-
-
+		# Tokens operadores aritmeticos, booleanos, relacionales, o de otro tipo
+		'TkSuma',               # "+"
+		'TkResta',              # "-"
+		'TkMult',               # "*"
+		'TkDiv',                # "/"
+		'TkMod',                # "%"
+		'TkConjuncion',         # "/\"
+		'TkDisyuncion',         # "\/"
+		'TkMenor',              # "<"
+		'TkMenorIgual',         # "<="
+		'TkMayor',              # ">"
+		'TkMayorIgual',         # ">="
+		'TkIgual',              # "="
+		'TkSiguienteChar',      # "++"
+		'TkAnteriorChar',       # "--"
+		'TkValorAscii',         # "#"
+		'TkConcatenacion',      # "::"
+		'TkShift',              # "$"
+	] + list(reserved.values())
 
 
-# Reglas para las expresiones regulares de tokens simples de sepradores,
-# aritmeticos, booleanos, relacionales, etc
-
-t_TkHacer 			= r'\-\>'
-t_TkAsignacion 		= r'\<\-'
-t_TkSiguienteChar 	= r'\+\+'
-t_TkSuma 			= r'\+'
-TkAnteriorChar 		= r'\-\-'
-t_TkResta 			= r'\-'
-t_TkMult 			= r'\*'
-t_TkConjuncion 		= r'\/\\'
-t_TkDisyuncion 		= r'\\\/'
-t_TkDiv 			= r'\/'
-t_TkMod 			= r'\%'
-t_TkNegacion 		= r'not' 
-t_TkMenorIgual 		= r'\<\='
-t_TkMenor 			= r'\<'
-t_TkMayorIgual 		= r'\>\='
-t_TkMayor 			= r'\>'
-t_TkIgual 			= r'\='
-t_TkValorAscii 		= r'\#'
-t_TkConcatenacion 	= r'\:\:'
-t_TkShift          	= r'\$'
-t_TkComa           	= r'\,'
-t_TkPunto          	= r'\.'
-t_TkDosPuntos      	= r'\:'
-t_TkParAbre        	= r'\('
-t_TkParCierra      	= r'\)'
-t_TkCorcheteAbre   	= r'\['
-t_TkCorcheteCierra 	= r'\]'
-t_TkLlaveAbre      	= r'\{'
-t_TkLlaveCierra    	= r'\}'
-
-#Regla de expresion regular para valores booleanos
-
-t_TkFalse = r'False'
-t_TkTrue  = r'True'
 
 
-# Regla para hacer match con un identificador y buscar 
-# entre las palabras reservadas
+	# Reglas para las expresiones regulares de tokens simples de sepradores,
+	# aritmeticos, booleanos, relacionales, etc
 
-def t_TkId(t):
-	r'[a-zA-Z_][a-zA-Z_0-9]*'
-	t.type = reserved.get(t.value,'TkId')    # Check for reserved words
-	return t
+	t_TkHacer 			= r'\-\>'
+	t_TkAsignacion 		= r'\<\-'
+	t_TkSiguienteChar 	= r'\+\+'
+	t_TkSuma 			= r'\+'
+	TkAnteriorChar 		= r'\-\-'
+	t_TkResta 			= r'\-'
+	t_TkMult 			= r'\*'
+	t_TkConjuncion 		= r'\/\\'
+	t_TkDisyuncion 		= r'\\\/'
+	t_TkDiv 			= r'\/'
+	t_TkMod 			= r'\%'
+	t_TkNegacion 		= r'not' 
+	t_TkMenorIgual 		= r'\<\='
+	t_TkMenor 			= r'\<'
+	t_TkMayorIgual 		= r'\>\='
+	t_TkMayor 			= r'\>'
+	t_TkIgual 			= r'\='
+	t_TkValorAscii 		= r'\#'
+	t_TkConcatenacion 	= r'\:\:'
+	t_TkShift          	= r'\$'
+	t_TkComa           	= r'\,'
+	t_TkPunto          	= r'\.'
+	t_TkDosPuntos      	= r'\:'
+	t_TkParAbre        	= r'\('
+	t_TkParCierra      	= r'\)'
+	t_TkCorcheteAbre   	= r'\['
+	t_TkCorcheteCierra 	= r'\]'
+	t_TkLlaveAbre      	= r'\{'
+	t_TkLlaveCierra    	= r'\}'
 
-# Regla para hacer match con un caracter
+	#Regla de expresion regular para valores booleanos
 
-def t_TkCaracter(t):
-    r"'[!-z]'"
-    return t 
+	t_TkFalse = r'False'
+	t_TkTrue  = r'True'
 
-# Imprimir los Tk_Caracter  
 
-def print_TkCaracter(t):
-    return "TkCaracter(" + t.value + ")" + " " + str(t.lineno) + " " + str(t.lexpos)
+	# Regla para hacer match con un identificador y buscar 
+	# entre las palabras reservadas
 
-# Funcion para imprimir un token del tipo nombre variable 
+	def t_TkId(self, t):
+		r'[a-zA-Z_][a-zA-Z_0-9]*'
+		t.type = self.reserved.get(t.value,'TkId')    # Check for reserved words
+		return t
 
-def print_TkId(t):
-    return 'TkId("' + t.value + '")' + " " + str(t.lineno) + " " + str(t.lexpos)
+	# Regla para hacer match con un caracter
 
-# Regla de expresion regular para los numeros
+	def t_TkCaracter(self, t):
+		r"'[!-z]'"
+		return t 
 
-def t_TkNum(t):
-	r'\d+'
-	t.value = int(t.value)    
-	return t
+	# Imprimir los Tk_Caracter  
 
-# Funcion para imprimir un token de tipo numero 
+	def print_TkCaracter(self, t):
+		return "TkCaracter(" + t.value + ")" + " " + str(t.lineno) + " " + str(t.lexpos)
 
-def print_TkNum(t):
-    return "TkNum(" + str(t.value) + ")" + " " + str(t.lineno) + " " + str(t.lexpos)
+	# Funcion para imprimir un token del tipo nombre variable 
 
-# Regla para definir el numero de la line
+	def print_TkId(self, t):
+		return 'TkId("' + t.value + '")' + " " + str(t.lineno) + " " + str(t.lexpos)
 
-def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += len(t.value)
+	# Regla de expresion regular para los numeros
 
-# Columnas
+	def t_TkNum(self, t):
+		r'\d+'
+		t.value = int(t.value)    
+		return t
 
-def find_column(input, token):
-	line_start = input.rfind('\n', 0, token.lexpos) + 1
-	return (token.lexpos - line_start) + 1
+	# Funcion para imprimir un token de tipo numero 
 
-# Regla para manejar errores
+	def print_TkNum(self, t):
+		return "TkNum(" + str(t.value) + ")" + " " + str(t.lineno) + " " + str(t.lexpos)
 
-def t_error(t):
-	print("Caracter ilegal '%s'" % t.value[0])
-	t.lexer.skip(1)
+	# Regla para definir el numero de la line
 
-# Regla de expresion regular de literales para caracteres
+	def t_newline(self, t):
+		r'\n+'
+		t.lexer.lineno += len(t.value)
 
-# literals = r'[(-=^\]+|,.)*"{}a-zA-Z_0-9'
+	# Columnas
 
-# Un String que contiene caracteres ignorados: espacios, tabuladores
-# y saltos de lineas
+	def find_column(self, input, token):
+		line_start = input.rfind('\n', 0, token.lexpos) + 1
+		return (token.lexpos - line_start) + 1
 
-t_ignore  = ' \t\n'
+	# Regla para manejar errores
 
-# Se construye el lexer
+	def t_error(self, t):
+		print("Caracter ilegal '%s'" % t.value[0])
+		t.lexer.skip(1)
 
-lexer = lex.lex()
+	# Regla de expresion regular de literales para caracteres
 
-data = '''
-with\nvar 7contador : char\nbegin\ncontador <- True .\nend\n
-'''
+	# literals = r'[(-=^\]+|,.)*"{}a-zA-Z_0-9'
 
-lexer.input(data)
+	# Un String que contiene caracteres ignorados: espacios, tabuladores
+	# y saltos de lineas
 
-# Variable que va a almacenar la salida del analizador 
-salida = ""
+	t_ignore  = ' \t\n'
 
-for tok in lexer:
-    if tok.type == 'TkNum':
-        salida = salida + " " + print_TkNum(tok)
-    elif tok.type == 'TkId':
-        salida = salida + " " + print_TkId(tok)
-    elif tok.type == 'TkCaracter':
-        salida = salida + " " + print_TkCaracter(tok)
-    else:
-        salida = salida + " " + tok.type + " " + str(tok.lineno) + " " + str(tok.lexpos)
+	# Se construye el lexer
 
-print(salida) 
+	def build(self, **kwargs):
+		self.lexer = lex.lex(module=self, **kwargs)
+
+	# Funcion para leer el archivo de entrada 
+	def read_archive(self, archive):
+		entrada = open(archive, "r")
+		string = entrada.read()
+		
+		print(string)
+		# Se llama a la funcion para testear el lexer 
+		return string
+
+	# Se prueba el tester
+	def test(self, data):
+		salida = ""
+		data = self.read_archive(data)
+		self.lexer.input(data)
+		for tok in self.lexer:
+			if tok.type == 'TkNum':
+				salida = salida + " " + self.print_TkNum(tok)
+			elif tok.type == 'TkId':
+				salida = salida + " " + self.print_TkId(tok)
+			elif tok.type == 'TkCaracter':
+				salida = salida + " " + self.print_TkCaracter(tok)
+			else:
+				salida = salida + " " + tok.type + " " + str(tok.lineno) + " " + str(tok.lexpos)
+		return salida 
